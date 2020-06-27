8fd8eb40de08caf19b94b44378344c21eba6abcb
محمد احمد اباالخيل -541 <kuwaitigamer2004@gmail.com>
2020-06-27 20:20:46 +0300

    done after a hard work

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,34 @@
 // السؤال الأول
 // 1
-
+let name = "mohamad";
 // 2
-
+let Age = 16;
 // 3 and 4
-
+console.log("نا" + " " + name + " " + " وعمري" + " " + Age);
 // 5 and 6
-
+console.log(Age * 6);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+const hoppies = ["reading, eating, pc gaming,"];
 // 2
-
+hoppies.push("sleaping");
 // 3
-
+hoppies.pop();
 // 4
 
 // 5
-
+let web = {
+    favoritlang: "html",
+    msary: "web devolopment",
+    teachers: "menawer",
+    ta: ["moudhi, nansy, rahaf, hussain, khajah"],
+};
 // 6
-
+console.log(web.msary);
 // 7
-
+console.log(web.ta);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
